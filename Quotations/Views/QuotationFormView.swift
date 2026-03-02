049a4420a73b37b6f7097f29c9fa7829d2df3427
Russell McWhae <russell.mcwhae@gmail.com>
2026-03-01 21:02:42 -0800

    Focus the form

--- Quotations/Views/QuotationFormView.swift
+++ Quotations/Views/QuotationFormView.swift
@@ -37,6 +37,7 @@ struct QuotationFormView: View {
                     }
                 }
             }
+            .onAppear { isFocused = true }
     }
 
     private func commitIfNonEmpty() {
